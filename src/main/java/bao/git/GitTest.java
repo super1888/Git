4c6f971576c13144e060e965f64bc409d7c5540b
2944 <294419455@qq.com>
2021-10-29 09:32:26 +0800

    hit-fix commit second

--- src/main/java/bao/git/GitTest.java
+++ src/main/java/bao/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git00000!");
+        System.out.println("hello hot-fix test!");
     }
 }
